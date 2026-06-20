8a15673d54f357b4564fdebb27764c7eaccd166b
unknown <sarmientodavidhans@gmail.com>
2026-06-20 15:33:24 +0800

    Add discussion code for day2 and day3

---
+++ src/day2/discussion/SampleCode.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day2.discussion;
+
+/**
+ *
+ * @author Admin
+ */
+public class SampleCode {
+    public static void main(String[] args) { //main method
+        System.out.println("Hello World!"); //print statement
+}
+}
+// Main Method -> starting point of programs execution
+// Main Method (psvm) -> starting point of programs execution
+// Print Statement (sout) -> it tells the computer to print something
+
+/*
+    2 Types of Comments
+    1. Single Line Comment
+    2. Multi Line Comment
+*/
