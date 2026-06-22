8bbe640d6709af75f84484e723dcc48f083602b2
davidhans27 <sarmientodavidhans@gmail.com>
2026-06-22 17:11:11 +0800

    initial commit

---
+++ src/day4/discussion/variable.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day4.discussion;
+
+/**
+ *
+ * @author Admin
+ */
+public class variable {
+    public static void main(String[] args) {
+        //initialisation
+        //DT    var     value
+        String timba = "tubig";
+        
+        timba = "bigas";
+        System.out.println("timba");
+        
+        //26
+        int age = 26;
+        System.out.println("Age: "+ age);
+        
+        
+        //38.5
+        //declaration
+        double temp;
+        
+        
+        //assignment
+       temp = 38.5;
+    }
+}
