9c3bcc412fdd7a7ccd66b6549ed8af4e23040e23
davidhans27 <sarmientodavidhans@gmail.com>
2026-06-23 14:07:01 +0800

    initial commit

--- src/day4/activity/Activity1_Sarmiento.java
+++ src/day4/activity/Activity1_Sarmiento.java
@@ -1,36 +1,24 @@
-// File name: Activity_YourLastName.java
-
 public class Activity1_Sarmiento {
 
     public static void main(String[] args) {
-
         // boolean - true or false
         boolean isJavaFun = true;
-
-        // byte - small integer (-128 to 127)
+        // byte
         byte byteValue = 100;
-
-        // short - small/medium integer
+        // short 
         short shortValue = 32000;
-
         // int - regular integer
         int intValue = 123456;
-
         // long - large integer (L suffix required)
         long longValue = 12345678910L;
-
         // float - decimal number (F suffix required)
         float floatValue = 12.34F;
-
         // double - larger decimal number
         double doubleValue = 12345.6789;
-
         // char - single character
         char charValue = 'J';
-
         // String - text
         String stringValue = "Hello Java World";
-
         // Output all values
         System.out.println("Boolean value: " + isJavaFun);
         System.out.println("Byte value: " + byteValue);
